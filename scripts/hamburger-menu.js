047f9273b2a963067dca890ea8f57a0981c8cb4d
musichubtech <boulevardsud.booking@gmail.com>
2024-12-19 21:21:41 +0100

    Update hamburger-menu.js

--- scripts/hamburger-menu.js
+++ scripts/hamburger-menu.js
@@ -1,6 +1,9 @@
-
-// Toggle Menu Function
 function toggleMenu() {
     const navMenu = document.getElementById('navMenu').querySelector('ul');
-    navMenu.classList.toggle('show');
+    if (navMenu) {
+        console.log('Menu trouvé');
+        navMenu.classList.toggle('show');
+    } else {
+        console.error('Menu non trouvé');
+    }
 }
